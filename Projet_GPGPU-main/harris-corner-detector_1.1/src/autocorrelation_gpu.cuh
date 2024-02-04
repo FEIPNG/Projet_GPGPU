fc6b17fe182017ab1b8abeb56960c3b291936333
mengfe <toutou@toutoubaobeideMacBook-Pro.local>
2024-02-04 17:59:50 +0100

    texture memeory

--- Projet_GPGPU-main/harris-corner-detector_1.1/src/autocorrelation_gpu.cuh
+++ Projet_GPGPU-main/harris-corner-detector_1.1/src/autocorrelation_gpu.cuh
@@ -1,2 +1,3 @@
 #pragma once
-void  compute_autocorrelation_matrix_cuda(float *Ix, float *Iy, float *A, float *B, float *C, int nx, int ny) ;
+// void  compute_autocorrelation_matrix_cuda(float *Ix, float *Iy, float *A, float *B, float *C, int nx, int ny) ;
+void  compute_autocorrelation_matrix_cuda_texture(float *Ix, float *Iy, float *A, float *B, float *C, int nx, int ny) ;
