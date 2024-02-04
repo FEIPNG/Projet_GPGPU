22624c7d5f79cdc1ff8d8e94ce408ab03fbebc74
mengfe <toutou@toutoubaobeideMacBook-Pro.local>
2024-02-04 18:59:30 +0100

    fix pb texture

--- Projet_GPGPU-main/harris-corner-detector_1.1/src/autocorrelation_gpu.cu
+++ Projet_GPGPU-main/harris-corner-detector_1.1/src/autocorrelation_gpu.cu
@@ -9,8 +9,8 @@ __global__ void compute_autocorrelation_matrix_kernel_texture(float *A, float *B
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     int p = x + y * blockDim.x * gridDim.x;
 
-    float Ix_value = tex1D(texIx, p);  // Read from texture memory
-    float Iy_value = tex1D(texIy, p);
+    float Ix_value = tex1Dfetch(texIx, p);  // Read from texture memory
+    float Iy_value = tex1Dfetch(texIy, p);
 
     A[p] = Ix_value * Ix_value;
     B[p] = Ix_value * Iy_value;
@@ -33,42 +33,42 @@ __global__ void compute_autocorrelation_matrix_kernel_texture(float *A, float *B
 
 //avec texture
 void compute_autocorrelation_matrix_cuda(float *Ix, float *Iy, float *A, float *B, float *C, int nx, int ny){
-    int imageSize = nx * ny;
+     int imageSize = nx * ny;
     // Allocate device memory
-    float *d_A, *d_B, *d_C;
+    float *d_Ix, *d_Iy, *d_A, *d_B, *d_C;
 
-    // Texture
-    cudaArray *d_IxArray, *d_IyArray;
-    cudaMallocArray(&d_IxArray, &texIx.channelDesc, imageSize, 1);
-    cudaMallocArray(&d_IyArray, &texIy.channelDesc, imageSize, 1);
-    // Copy input data to texture memory
-    cudaMemcpyToArray(d_IxArray, 0, 0, Ix, imageSize * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpyToArray(d_IyArray, 0, 0, Iy, imageSize * sizeof(float), cudaMemcpyHostToDevice);
-
-    // Bind texture references to the arrays
-    cudaBindTextureToArray(texIx, d_IxArray);
-    cudaBindTextureToArray(texIy, d_IyArray);
-
+    cudaMalloc((void**)&d_Ix, imageSize * sizeof(float));
+    cudaMalloc((void**)&d_Iy, imageSize * sizeof(float));
     cudaMalloc((void**)&d_A, imageSize * sizeof(float));
     cudaMalloc((void**)&d_B, imageSize * sizeof(float));
     cudaMalloc((void**)&d_C, imageSize * sizeof(float));
     
+    size_t offset = 0;
+
+    texInput_x.addressMode[0] = cudaAddressModeBorder;
+    texInput_x.addressMode[1] = cudaAddressModeBorder;
+    texInput_x.filterMode = cudaFilterModePoint;
+    texInput_x.normalized = false;
+    texInput_y.addressMode[0] = cudaAddressModeBorder;
+    texInput_y.addressMode[1] = cudaAddressModeBorder;
+    texInput_y.filterMode = cudaFilterModePoint;
+    texInput_y.normalized = false;
+    // copy in the memory
+    cudaMemcpy(d_Ix, Ix, imageSize * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Iy, Iy, imageSize * sizeof(float), cudaMemcpyHostToDevice);
+    cudaBindTexture(&offset, texInput_x, d_Ix, sizeof(float)*imageSize);
+    cudaBindTexture(&offset, texInput_y, d_Iy, sizeof(float)*imageSize);
     // Define block and grid dimensions
     dim3 blockSize(10, 10); // Adjust block size based on your GPU architecture
     dim3 gridSize((int)(nx/blockSize.x), (int)(ny / blockSize.y));
     // Launch the kernel
     compute_autocorrelation_matrix_kernel_texture<<<gridSize, blockSize>>>(d_A, d_B, d_C);
-
     // Wait for kernel completion
     cudaDeviceSynchronize();
     // Copy the results back to host
     cudaMemcpy(A, d_A, imageSize * sizeof(float), cudaMemcpyDeviceToHost);
     cudaMemcpy(B, d_B, imageSize * sizeof(float), cudaMemcpyDeviceToHost);    
     cudaMemcpy(C, d_C, imageSize * sizeof(float), cudaMemcpyDeviceToHost);
-
-    // Unbind texture references
-    cudaUnbindTexture(texIx);
-    cudaUnbindTexture(texIy);
     // printf("A:\n");
     // for (int j = 0; j < 100; j++){
 	// 	printf("%f",A[j]);
@@ -82,11 +82,11 @@ void compute_autocorrelation_matrix_cuda(float *Ix, float *Iy, float *A, float *
 	// 	printf("%f",C[j]);
 	// }
     // Free device memory
+    cudaFree(d_Ix);
+    cudaFree(d_Iy);
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
-    cudaFreeArray(d_IxArray);
-    cudaFreeArray(d_IyArray);
 }
 
 // sans texture 
